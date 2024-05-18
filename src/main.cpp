625b21ff6d77b7a12e0d62ec5bde150bdb6d13f9
Mason <masonhwolf@gmail.com>
2024-05-18 14:09:57 -0500

    added notes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * Author: Mason Wolf
+ * Date: May 18, 2024
+ * https://github.com/mason-wolf/cpp-sandbox
+ * 
+ * A simple text-based game.
+ * Failed project, but lots of lessons learned.
+ * Some useful tidbits here include file IO, hashmaps, spliting strings.
+ * Data structure in the game_data.txt just doesn't work correctly.
+ * Should've thought it through better before writing all the parsing logic.
+*/
 #include <iostream>
 #include "game_data.h"
 #include "game.h"
